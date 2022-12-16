9b5d35206145e6dcd3bc5172825c314ac69e41df
ryantait83 <taitr@uoguelph.ca>
2022-12-16 17:31:59 -0500

    kkl

--- newlab6.c
+++ newlab6.c
@@ -6,8 +6,7 @@ int main(void)
     printf("Enter 'y' and press enter if you know how to use Git!\n");
     scanf(" %c", &val);
 
-    if(val == 'y')
-    {
+    if(val == 'y'){
         printf("You know how to use Git!");
     }
     else
